1205df259b6ae5420ae87b78a0cd15cad690707b
Prasanna Samarasinghe <prasannads@gmail.com>
2018-04-07 09:41:55 +0530

    first Commit
    first Commit

---
+++ Sudent Registration/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Student_Registration.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string StudNumber { get; set; }
+        public string StrudLastName { get; set; }
+        public string StudFirstName { get; set; }
+        public int StudAge { get; set; }
+        public DateTime Stud_DOB { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
---
+++ Sudent Registration/Models/StudentInitializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace Student_Registration.Models
+{
+    public class StudentInitializer:DropCreateDatabaseIfModelChanges<SchoolContext>
+    {
+        protected override void Seed(SchoolContext context)
+        {
+            var students = new List<Student>
+            {
+                new Student{ StudNumber = "STU001", StrudLastName ="Ceena", StudFirstName="Jhon", StudAge=43, CreatedOn=System.DateTime.Now, CreatedBy="Prasanna"},
+                new Student{ StudNumber = "STU002", StrudLastName ="Cross Bone", StudFirstName="Larry", StudAge=44, CreatedOn=System.DateTime.Now, CreatedBy="Prasanna"},
+                new Student{ StudNumber = "STU003", StrudLastName ="Big Ass", StudFirstName="Jane", StudAge=23, CreatedOn=System.DateTime.Now, CreatedBy="Prasanna"},
+                new Student{ StudNumber = "STU004", StrudLastName ="Fisher", StudFirstName="Doge", StudAge=27, CreatedOn=System.DateTime.Now, CreatedBy="Prasanna"},
+                new Student{ StudNumber = "STU005", StrudLastName ="Hamster", StudFirstName="Michel", StudAge=19, CreatedOn=System.DateTime.Now, CreatedBy="Prasanna"}
+
+
+            };
+
+            foreach(var temp in students)
+            { 
+            context.TBL_Students.Add(temp);
+            }
+
+            context.SaveChanges();
+
+
+        }
+
+    }
+}

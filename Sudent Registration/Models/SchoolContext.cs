5ef4753f6d20d1476bd49cf3a91dc8523a2fd321
Prasanna Samarasinghe <prasannads@gmail.com>
2018-04-07 09:52:20 +0530

    second commit
    secon

---
+++ Sudent Registration/Models/SchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using Student_Registration.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Student_Registration.Models
+{
+    public class SchoolContext:DbContext
+
+    { 
+
+        public DbSet<Student> TBL_Students { get; set; }
+    }
+}
